5869fc898f828233b163cd69c316855e19c0c09c
Tom Molesworth <tom@perlsite.co.uk>
2013-08-18 13:21:28 +0000

    More local examples
    git-svn-id: http://entitymodel.com/repo/trunk/Tickit/Tickit-Desktop@366 c09ddf91-2ef0-45e8-8c68-47ccf8e3121d

---
+++ examples/imap.pl
@@ -0,0 +1,110 @@
+#!/usr/bin/env perl 
+use strict;
+use warnings;
+use Heap;
+use IO::Async::Loop::Epoll;
+use IO::Async::Loop;
+use Tickit::Async;
+use Tickit::Widget::VBox;
+use Tickit::Widget::Menubar;
+use Tickit::Widget::Menubar::Item;
+use Tickit::Widget::Desktop;
+use Tickit::Widget::Statusbar;
+
+#use Carp::Always;
+
+my $loop = IO::Async::Loop->new;
+$loop->add(my $tickit = Tickit::Async->new);
+my $vbox = Tickit::Widget::VBox->new;
+
+# Defer the setup until we have our terminal window, since we're not currently
+# hooking the ::Desktop window allocation.
+$loop->later(sub {
+	$vbox->add(my $mb = Tickit::Widget::Menubar->new(
+		popup_container => $vbox->window,
+		linetype => 'single',
+	));
+	$vbox->add(my $desktop = Tickit::Widget::Desktop->new(loop => $loop), expand => 1);
+
+	# None of these do anything since we're not providing any actions
+	$mb->add_item(my $item = Tickit::Widget::Menubar::Item->new(label => '&File'));
+	$item->add_item(Tickit::Widget::Menubar::Item->new(
+		label => 'E&xit',
+		on_activate => sub {
+			$tickit->later(sub {
+				$tickit->stop;
+			})
+		}
+	));
+	$mb->add_item($item = Tickit::Widget::Menubar::Item->new(label => '&Widgets'));
+	my $demo = Tickit::Demo->new(
+		desktop => $desktop,
+		tickit => $tickit,
+		loop => $loop,
+	);
+	foreach my $plugin ($demo->plugins(demo => $demo)) {
+		$item->add_item($plugin->menu_item($desktop));
+	}
+	$item->add_item(Tickit::Widget::Menubar::Item->new(
+		label => 'Show me everything',
+		on_activate => sub {
+			foreach my $plugin ($demo->plugins(demo => $demo)) {
+				my $w = $plugin->widget or die "No widget for $plugin?";
+				my $left = int($desktop->window->cols * rand);
+				my $top = int($desktop->window->lines * rand);
+				my $cols = 20 + int(10 * rand);
+				my $lines = 5 + int(20 * rand);
+				$left = $desktop->window->cols - $cols if $left + $cols >= $desktop->window->cols;
+				$top = $desktop->window->lines - $lines if $top + $lines >= $desktop->window->lines;
+				$desktop->create_panel(
+					label => $plugin->label,
+					left => $left,
+					top => $top,
+					cols => $cols,
+					lines => $lines,
+				)->add($w);
+			}
+		},
+	));
+	$mb->add_item($item = Tickit::Widget::Menubar::Item->new(label => 'Win&dows'));
+	$item->add_item(Tickit::Widget::Menubar::Item->new(
+		label => '&Tile',
+		on_activate => sub { $desktop->tile },
+	));
+	$item->add_item(Tickit::Widget::Menubar::Item->new(
+		label => 'Ca&scade',
+		on_activate => sub { $desktop->cascade }
+	));
+	$item->add_item(Tickit::Widget::Menubar::Item->new(
+		label => 'Over&lapping tiles',
+		on_activate => sub { $desktop->tile(overlap => 1) }
+	));
+	$item->add_item(Tickit::Widget::Menubar::Item->new(
+		label => 'Close a&ll',
+		on_activate => sub { $desktop->close_all }
+	));
+	$mb->add_item($item = Tickit::Widget::Menubar::Item::Separator->new, expand => 1);
+	$mb->add_item($item = Tickit::Widget::Menubar::Item->new(label => '&Help'));
+	$item->add_item(Tickit::Widget::Menubar::Item->new(
+		label => '&About',
+		on_activate => sub { }
+	));
+	$vbox->add(
+		Tickit::Widget::Statusbar->new(loop => $loop)
+	);
+});
+$tickit->set_root_widget($vbox);
+$tickit->run;
+
+__END__
+
+=pod
+
+File
+
+Edit
+* Manage accounts
+* 
+
+=cut
+
